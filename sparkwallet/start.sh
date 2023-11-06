72fd8708271527f0c9fcd5f8143cca9d0f0dab42
Aljaz Ceru <aljaz@ceru.si>
2023-11-06 21:08:21 +0100

    docker 2.0 update

--- sparkwallet/start.sh
+++ sparkwallet/start.sh
@@ -19,5 +19,5 @@ fi
 if [ "$DOCKER_MODE" = "swarm" ]; then
   docker stack deploy -c $APP_SCRIPT_PATH/docker-compose.yaml $APP_ID
 elif [ "$DOCKER_MODE" = "compose" ]; then
-  docker-compose -f $APP_SCRIPT_PATH/docker-compose.yaml up -d --remove-orphans
+  docker compose -f $APP_SCRIPT_PATH/docker-compose.yaml up -d --remove-orphans
 fi
